c534ade33220a26f40b868830af4a6ed7e00e2dd
weiba <lh1738289913@gmail.com>
2018-12-04 15:31:15 +0800

    Update pTest.sh
    更新url提示

--- pTest.sh
+++ pTest.sh
@@ -2,24 +2,29 @@
 # export PATH=$PATH:/bin:/sbin:/usr/sbin
 
 if [ ! -n "$1" -o ! -n "$2" ]; then
-    echo "请在脚本后输入参数： 进程端口号 目标端口号"
+    echo "请在脚本后输入参数：注册中心端口号 目标服务端口号"
     exit 1
 fi
 
-id=$(netstat -tunlp | grep $1 | awk '{print $7}' | awk -F '/' '{print $1}')
+PID=$(netstat -tunlp | grep $1 | awk '{print $7}' | awk -F '/' '{print $1}')
 
-echo "端口$1所属进程id为：${id}"
+echo "端口$1所属进程PID为：${PID}"
 
-ip=$(lsof -p ${id}  -nP | grep TCP | awk '{print $9}' | awk -F '->' '{print $2}' | awk -F ':' '{print $1}'| sort| uniq | grep -v '^$')
+ips=$(lsof -p ${PID}  -nP | grep TCP | awk '{print $9}' | awk -F '->' '{print $2}' | awk -F ':' '{print $1}' | grep -v '^$' | sort | uniq)
 
-for i in ${ip}; do
- #cr=$(curl -I -m 10 -o /dev/null -s -w %{http_code} http://${i}:$2/swagger-ui.html)
- #echo $i ${cr}
+for i in ${ips}; do
  nc -z -w 2 ${i} $2 >> /dev/null 2>&1
  result=$?
  if [ ${result} != 0 ]; then
-	echo -e "${i}:$2 \t 端口关闭"
+        echo -e "${i}:$2 \t 端口关闭"
  else
-	echo -e "${i}:$2 \t 端口开放 <-- http://${i}:$2/swagger-ui.html"
+        swagger="http://${i}:$2/swagger-ui.html"
+        cr=$(curl -I -m 10 -o /dev/null -s -w %{http_code} ${swagger})
+        if [ ${cr} != 200 ]; then
+           desUrl="manager - http://${i}:$2/"
+        else
+           desUrl="microservice - ${swagger}"
+        fi
+        echo -e "${i}:$2 \t 端口开放 <-- ${desUrl}"
  fi
 done
